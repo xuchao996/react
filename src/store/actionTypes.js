40edb2c991b3e6570adc4153f3722b789029fe66
xuchao996 <44614061+xuchao996@users.noreply.github.com>
2019-08-29 18:46:45 +0800

    axios + commet
    axios + commet

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,3 +1,6 @@
+// 定义统一的名字做action type
+
 export const CHANGE_INPUT = "changeInput";
 export const ADD_ITEM = "addItem";
 export const DELETE_ITEM = "deleteItem";
+export const GET_LIST = "getList";
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 
 import reducer from "./reducers/index";
 
+// store 必须是唯一的
 const store = createStore(reducer);
 
 export default store;
